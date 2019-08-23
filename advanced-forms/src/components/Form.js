bf972a7a7016923b70bcfe5d72e6b069a57ecf5f
unknown <dereketman@gmail.com>
2019-08-22 21:16:24 -0700

    major styling

--- advanced-forms/src/components/Form.js
+++ advanced-forms/src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Form, withFormik, Field } from 'formik';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import * as Yup from "yup";
 
@@ -17,23 +17,32 @@ const OnboardForm = ({errors, touched, values, status}) => {
 
     return(
      <div className="onboardForm">
+      <div className="innerBox">
+          <div className="titleBox">
+            <h1> ENROLL FOR REWARDS.</h1>
+            <h2> Get more when you spend more.</h2>
+         </div>
             <Form>
-                <Field type="text" name="name" placeholder="Name" />
                 {touched.name && errors.name && (
-            <p className="error">{errors.name}</p>
-            )}
-                <Field type="email" name="email" placeholder="Email" />
+                <p className="error">{errors.name}</p>
+                )}
+                <Field className="formComponent" type="text" name="name" placeholder="Name" />
+
                 {touched.email && errors.email && (
-            <p className="error">{errors.email}</p>
-            )}
-                <Field type="password" name="password" placeholder="password" />
+                <p className="error">{errors.email}</p>
+                )}
+                <Field className="formComponent" type="email" name="email" placeholder="Email" />
+
                 {touched.password && errors.password && (
-            <p className="error">{errors.password}</p>
-            )}
+                <p className="error">{errors.password}</p>
+                )}
+                <Field className="formComponent" type="password" name="password" placeholder="Password" />
+
 
                 <label className="checkbox-container">
                     Terms of Service
                     <Field 
+                    className="tos"
                     type="checkbox"
                     name="termsOfService"
                     checked={values.termsOfService}
@@ -41,7 +50,7 @@ const OnboardForm = ({errors, touched, values, status}) => {
                     <span className="checkmark" />
                 </label>
 
-                <button type="submit">Submit</button>
+                <button type="submit">Join Now</button>
             </Form>
 
          <Container>
@@ -52,6 +61,7 @@ const OnboardForm = ({errors, touched, values, status}) => {
                 </div>
             ))}
          </Container>
+      </div>
     </div>
     );
 };
@@ -69,7 +79,7 @@ const FormikOnboardForm = withFormik({
         name: Yup.string().required("Please enter your name"),
         email: Yup.string().required("Please enter your email"),
         password: Yup.string().required("Please enter your password"),
-        termsOfService: Yup.boolean().oneOf([true], "Please agree")
+        termsOfService: Yup.boolean().oneOf([false], "Please agree")
     }),
     handleSubmit( values, { setStatus } ){
         axios
